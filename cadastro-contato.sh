bfdec4efdc65c23cacd3cc566dfd95f9a80147b6
Kézia Ketillen <88369589+Kk3tillen@users.noreply.github.com>
2024-01-18 02:05:09 -0300

    Update cadastro-contato.sh

--- cadastro-contato.sh
+++ cadastro-contato.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Captura do nome usando inputbox
 nome=$(dialog --inputbox "Nome do contato:" 10 40 3>&1 1>&2 2>&3)
 
-# Captura do número de telefone usando inputbox
 telefone=$(dialog --inputbox "telefone:" 10 40 3>&1 1>&2 2>&3)
 
-# Exibe os dados inseridos
 dialog --title "Informações inseridas" --msgbox "Nome: $nome\nTelefone: $telefone" 12 30
 
 echo "$nome $telefone" >> ~/simple-phonebook/contatos.txt
